78aff3e8e950a8042b7ed0de45e7222114d960a0
yayang12 <yayang1@s.sfusd.edu>
2023-01-12 12:30:47 -0800

    Copied files over

---
+++ Assets/scripts/Propeller.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Propeller : MonoBehaviour
+{
+    private float rotationSpeed = 500f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime);
+    }
+}
